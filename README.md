a3287dcfb69a36d91bbb4175136efeb1ffe827c4
Yehuda Freedman <hudafreedman@gmail.com>
2024-05-07 01:32:53 +0300

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# vue-template
+
+this is my personal template for starting a new vue project
+
+it follows my prefered directory structure and use Tailwind
+
+## Installation
+
+```sh
+git clone {repo url } {project name}
+cd {project name}
+npm install
+npm run dev
+```
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <div class="flex items-center justify-center w-screen h-screen">
+      <div
+        class="flex flex-col items-center justify-center gap-2 p-10 border shadow">
+        <h1 class="text-6xl">Welcome</h1>
+        <p>Your new Vue project is ready</p>
+
+        <ul class="p-4 list-disc list-inside border">
+          <h2 class="text-2xl">Installed Libraries</h2>
+          <li>Vue-router</li>
+          <li>Vue-DevTools</li>
+          <li>Tailwind</li>
+        </ul>
+      </div>
+    </div>
+    <p class="fixed bottom-0 w-full p-2 text-center">
+      Made with 🖤 by
+      <a href="https://github.com/spaceCabbage" class="underline"
+        >SpaceCabbage</a
+      >
+    </p>
+  </main>
+</template>
